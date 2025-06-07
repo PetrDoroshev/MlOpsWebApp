10451f0fa51dcba7bc84f1f3d34193a75ff6b511
Petr Doroshev <doroshevpetr@gmail.com>
2025-06-07 21:35:58 +0300

    very simple youtube uploading

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, Request
+import os
+import uuid
+
+import uvicorn
+from fastapi import FastAPI, UploadFile, File, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import shutil
 import librosa
 from threading import Lock
+import yt_dlp
 
 from genre_model.processing.feature_extraction import extract_features
 from genre_model.predict import make_prediction, resolve_prediction
@@ -37,6 +42,36 @@ async def upload_audio(request: Request, file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
 
     audio, sr = librosa.load(file_location)
+
+    features_data = extract_features(audio, sr)
+    result = make_prediction(input_data=features_data)
+    print(result)
+    str_result = resolve_prediction(input_data=result[0]["preds"][0])
+
+    return templates.TemplateResponse("result.html", {"request": request, "result": str_result})
+
+@app.post("/upload_yt_url/", response_class=HTMLResponse)
+async def upload_audio_youtube(request: Request, youtube_url: str = Form(...)):
+
+    print(youtube_url)
+
+    fileName = f"youtube_{uuid.uuid4().hex[:8]}"
+    output_path = os.path.join("uploaded_files/", f"{fileName}.mp3")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': output_path.replace('.mp3', '.%(ext)s'),
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.extract_info(youtube_url, download=True)
+
+    audio, sr = librosa.load( f"uploaded_files/{fileName}.mp3")
 
     features_data = extract_features(audio, sr)
     result = make_prediction(input_data=features_data)
